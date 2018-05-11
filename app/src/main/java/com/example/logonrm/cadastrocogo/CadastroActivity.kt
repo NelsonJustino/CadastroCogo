bb1d46437fad9ded9b8793c1721192ef2d6bd62b
logonrm <logonrm@fiap.com.br>
2018-05-11 09:45:26 -0300

    Initial commit

---
+++ app/src/main/java/com/example/logonrm/cadastrocogo/CadastroActivity.kt
@@ -0,0 +1,44 @@
+package com.example.logonrm.cadastrocogo
+
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_cadastro.*
+import kotlinx.android.synthetic.main.activity_sucesso_cadastro.*
+
+class CadastroActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro)
+        btLimpar.setOnClickListener {
+            limpar()
+        }
+
+
+        btCadastrar.setOnClickListener{
+            val telaSucesso = Intent (this, SucessoCadastroActivity::class.java)
+            //telaSucesso.putExtra("nome", etNome.text.toString()
+
+            val jogo = Jogo (
+                    etNome.text.toString(),
+                    etGenero.text.toString(),
+                    etAno.text.toString().toInt()
+
+            )
+            telaSucesso.putExtra("jogo",jogo)
+            startActivity(telaSucesso)
+            limpar()
+
+        }
+    }
+
+    fun limpar(){
+        etNome.setText("")
+        etGenero.setText("")
+        etAno.setText("")
+
+
+    }
+}
---
+++ app/src/main/java/com/example/logonrm/cadastrocogo/SucessoCadastroActivity.kt
@@ -0,0 +1,19 @@
+package com.example.logonrm.cadastrocogo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_sucesso_cadastro.*
+
+class SucessoCadastroActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sucesso_cadastro)
+        //tvMensagem.text = intent.getStringExtra("nome")
+        val jogo = intent.getParcelableExtra<Jogo>("jogo")
+        tvMensagem.text = getString(R.string.cadastro_com_sucesso,
+                jogo.nome,
+                jogo.genero,
+                jogo.ano)
+    }
+}
